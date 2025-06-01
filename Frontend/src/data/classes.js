9d6482f1089c4e3bc1d83c2d6a1d241b58656362
singla19 <bhavaysingla2006@gmail.com>
2025-06-01 16:03:55 +0530

    req for render deployment

--- Frontend/src/data/classes.js
+++
@@ -1,9 +0,0 @@
-const classes = [
-    { id: 1, name: 'Mathematics 101', students: 30 },
-    { id: 2, name: 'Mathematics 201', students: 25 },
-    { id: 3, name: 'Algebra 204', students: 20 },
-    { id: 4, name: 'Probability and Statistics 302', students: 28 },
-  ];
-  
-  export default classes;
-  
